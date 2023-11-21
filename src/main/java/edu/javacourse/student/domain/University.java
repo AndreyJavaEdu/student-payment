3bd7783a0d61c4e5510d1abf2638ee01b812cb16
AndreyJavaEd <scream123456@yandex.ru>
2023-11-21 23:55:53 +0300

    Создали класс доменной модели CountryArea и замапили на таблицу jc_country_struct. Создали Интерфейс Репозитория CountryAreaRepository. В классах доменной модели University.java и CountryArea.java, Street.java убрали аннотацию @GeneratedValue.

--- src/main/java/edu/javacourse/student/domain/University.java
+++ src/main/java/edu/javacourse/student/domain/University.java
@@ -9,7 +9,6 @@ import javax.persistence.*;
 public class University {
     @Id
     @Column(name = "university_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long universityId;
     @Column(name = "university_name")
     private String universityName;
