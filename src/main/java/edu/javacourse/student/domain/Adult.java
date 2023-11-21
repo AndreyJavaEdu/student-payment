8177f6517a2bb4177f9512d5a29ef9ce632450fc
AndreyJavaEd <scream123456@yandex.ru>
2023-11-21 22:09:58 +0300

    Расширили класс Person классом Adult для заполнения полей паспорта. Поэтому класс Person аннотировали @MappedSuperclass. Т.к. внедряемые объекты (Embedded) нельзя наследовать. В SQL скрипт добавили столбцы для паспорта pasport_seria, passport_number, passport_date. Для жены и мужа. Теперь в классе StudentOrder мы внедряем объекты husband и wife по типу Adult а не Person и аннотировали эти объекты аннотацией @Embedded. Добавили в переопределение атрибутов поля класса Adult: passportSerial, passportNumber, issueDate.
    В классе Сервиса переделали возвращаемое значение метода buildPerson на Adult и в самом методе уже создаем объект типа Adult и заполняем его.

--- src/main/java/edu/javacourse/student/domain/Adult.java
+++ src/main/java/edu/javacourse/student/domain/Adult.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
+import javax.persistence.Entity;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDate;
 @Getter
 @Setter
-@MappedSuperclass
+@Embeddable
 public class Adult extends Person {
     private String passportSerial;
     private String passportNumber;
--- src/main/java/edu/javacourse/student/domain/StudentOrder.java
+++ src/main/java/edu/javacourse/student/domain/StudentOrder.java
@@ -53,5 +53,4 @@ public class StudentOrder {
     @Embedded
     private Adult husband;
 
-
 }
