3b74c0ccb09b48dcb2fc4da4c732cd6c005a327c
AndreyJavaEd <scream123456@yandex.ru>
2023-11-24 14:05:08 +0300

    В классе доменной модели StudentOrderChild.java замапили все поля таблицы. Переопределили имена колонок из класса Person с помощью аннотации @AttributeOverride. Также переопределили имя колонки таблицы сущности Address - с_street_code с помощью аннотации @AssociationOverride. Добавили недостающие поля studentOrder - для колонки student_order_id. Добавили поле certificateNumber для колонки с_certificate_number.
    Добавили поле certificateDate для колонки с_certificate_date.
    Показали ForeignKey с помощью @ManyToOne на поле registerOffice по колонке с_register_office_id.

--- src/main/java/edu/javacourse/student/domain/StudentOrderChild.java
+++ src/main/java/edu/javacourse/student/domain/StudentOrderChild.java
@@ -3,10 +3,25 @@ package edu.javacourse.student.domain;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+
 @Data
 @Entity
 @Table(name = "jc_student_child")
-public class StudentOrderChild {
+@AssociationOverrides({
+        @AssociationOverride(name = "address.street", joinColumns = @JoinColumn(name = "с_street_code"))
+})
+@AttributeOverrides({
+        @AttributeOverride(name = "surName", column = @Column(name = "с_surname")),
+        @AttributeOverride(name = "givenName", column = @Column(name = "с_gсiven_name")),
+        @AttributeOverride(name = "patronymic", column = @Column(name = "с_patronomyc")),
+        @AttributeOverride(name = "dateOfBirthday", column = @Column(name = "с_date_of_birth")),
+        @AttributeOverride(name = "address.postCode", column = @Column(name = "с_post_index")),
+        @AttributeOverride(name = "address.building", column = @Column(name = "с_building")),
+        @AttributeOverride(name = "address.extension", column = @Column(name = "с_extension")),
+        @AttributeOverride(name = "address.apartment", column = @Column(name = "с_apartment"))
+})
+public class StudentOrderChild extends Person {
     @Id
     @Column(name = "student_child_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +31,14 @@ public class StudentOrderChild {
     @JoinColumn(name = "student_order_id")
     private StudentOrder studentOrder;
 
+    @Column(name = "с_certificate_number")
+    private String certificateNumber;
 
+    @Column(name = "с_certificate_date")
+    private LocalDate certificateDate;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    @JoinColumn(name = "с_register_office_id")
+    private RegisterOffice registerOffice;
+    
 }
