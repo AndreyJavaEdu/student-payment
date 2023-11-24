3cff818c5b914dd8fd2054202d0e033b110991e0
AndreyJavaEd <scream123456@yandex.ru>
2023-11-24 13:30:02 +0300

    В класс StudentOrder добавили отношение @OneToMany и сделали mappedBy на объект studentOrder из класса StudentOrderChild. Тем самым мы сделали отношение между таблицами jc_student_order и jc_student_child по столбцу student_order_id - показали, что это Foreign Key.

--- src/main/java/edu/javacourse/student/domain/StudentOrder.java
+++ src/main/java/edu/javacourse/student/domain/StudentOrder.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 
 @Setter
@@ -22,6 +23,10 @@ public class StudentOrder {
     private StudentOrderStatus studentOrderStatus;
     @Column(name = "student_order_date")
     private LocalDate studentOrderDate;
+
+    @OneToMany(cascade = {CascadeType.REFRESH, CascadeType.REMOVE}, fetch = FetchType.LAZY,
+    mappedBy = "studentOrder")
+    private List<StudentOrderChild> children;
 
     @AssociationOverrides({
             @AssociationOverride(name = "address.street", joinColumns = @JoinColumn(name = "w_street_code")),
